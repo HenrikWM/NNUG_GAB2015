b38fe44b905d7e353e1509a5b06ac4b1a2645f4f
Henrik W. M. <henrik.moe@bouvet.no>
2015-04-10 15:38:07 +0200

    Assignment 2: Renamed project files

---
+++ GAB.OrderConsumer/OrderStorage.cs
@@ -0,0 +1,14 @@
+﻿namespace GAB.OrderConsumer
+{
+    using System.Collections.Generic;
+
+    using GAB.Domain;
+
+    public class OrderStorage
+    {
+        public void Store(List<Order> order)
+        {
+            
+        }
+    }
+}
